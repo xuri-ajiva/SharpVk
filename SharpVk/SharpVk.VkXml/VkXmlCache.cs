83dd892076f488b3ea47a4c22ef8a1c21c1f0075
Andrew Armstrong <facticiusvir@gmail.com>
2016-03-15 18:12:29 +0000

    Added SharpVk.VkXml project

---
+++ SharpVk/SharpVk.VkXml/VkXmlCache.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Xml.Linq;
+
+namespace SharpVk.VkXml
+{
+    public class VkXmlCache
+    {
+        private readonly string tempFilePath;
+
+        public VkXmlCache(string tempFilePath)
+        {
+            this.tempFilePath = tempFilePath;
+
+            if(!Directory.Exists(this.tempFilePath))
+            {
+                Directory.CreateDirectory(this.tempFilePath);
+            }
+        }
+
+        public XDocument GetVkXml()
+        {
+            string tempFile = Path.Combine(this.tempFilePath, "vk.xml");
+
+            if (!File.Exists(tempFile) || File.GetLastWriteTimeUtc(tempFile) + TimeSpan.FromDays(1) < DateTime.UtcNow)
+            {
+                try
+                {
+                    Directory.CreateDirectory(Path.GetDirectoryName(tempFile));
+
+                    var vkXmlRequest = WebRequest.Create("https://raw.githubusercontent.com/KhronosGroup/Vulkan-Docs/1.0/src/spec/vk.xml");
+
+                    using (var vkXmlResponse = vkXmlRequest.GetResponse())
+                    using (var fileStream = File.OpenWrite(tempFile))
+                    {
+                        vkXmlResponse.GetResponseStream().CopyTo(fileStream);
+                    }
+                }
+                catch
+                {
+                }
+            }
+
+            return XDocument.Load(tempFile);
+        }
+    }
+}
---
+++ SharpVk/SharpVk/ResultUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SharpVk
+{
+    public static class ResultUtil
+    {
+        public static void HandleResult(Result vkResult)
+        {
+            if ((int)vkResult < 0)
+            {
+                throw new Exception("Function returned error result: " + vkResult);
+            }
+        }
+    }
+}
