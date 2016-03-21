f7e0413bf4b3d9a4cd67a84d44f60df5a388b38a
FacticiusVir <FacticiusVir@gmail.com>
2016-03-21 01:26:07 +0000

    Implemented Instance constructor.

--- SharpVk/SharpVk/Instance.cs
+++ SharpVk/SharpVk/Instance.cs
@@ -20,20 +20,27 @@
 //OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //SOFTWARE.
 
-public class Instance
-{
-	/*<command successcodes="VK_SUCCESS" errorcodes="VK_ERROR_OUT_OF_HOST_MEMORY,VK_ERROR_OUT_OF_DEVICE_MEMORY,VK_ERROR_INITIALIZATION_FAILED,VK_ERROR_LAYER_NOT_PRESENT,VK_ERROR_EXTENSION_NOT_PRESENT,VK_ERROR_INCOMPATIBLE_DRIVER">
-  <proto>
-    <type>VkResult</type>
-    <name>vkCreateInstance</name>
-  </proto>
-  <param>const <type>VkInstanceCreateInfo</type>* <name>pCreateInfo</name></param>
-  <param optional="true">const <type>VkAllocationCallbacks</type>* <name>pAllocator</name></param>
-  <param>
-    <type>VkInstance</type>* <name>pInstance</name></param>
-</command>*/
 
+namespace SharpVk
+{
+	public class Instance
+	{
+		private Interop.Instance handle;
+
+		public Instance(InstanceCreateInfo createInfo, AllocationCallbacks allocator)
+		{
+			unsafe
+			{
+				fixed(Interop.Instance* handlePointer = &this.handle)
+				{
+					Result createResult = Interop.NativeMethods.vkCreateInstance(createInfo != null ? createInfo.instance : null, allocator != null ? allocator.instance : null, handlePointer);
+
+					ResultUtil.HandleResult(createResult);
+				}
+			}
+		}
 	//vkDestroyInstance Instance
 	//vkEnumeratePhysicalDevices Instance
 	//vkGetInstanceProcAddr Instance
+	}
 }
