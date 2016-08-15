7a1275a61ad757fffa9b1c64ec27fdd2774c9cf8
Andrew Armstrong <FacticiusVir@gmail.com>
2016-08-15 23:13:10 +0100

    Started work on Generator project

---
+++ SharpVk/SharpVk.Generator/Program.cs
@@ -0,0 +1,21 @@
+﻿using SharpVk.VkXml;
+using System;
+
+namespace SharpVk.Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xmlCache = new VkXmlCache(".\\VkTemplates");
+
+            var parser = new SpecParser(xmlCache);
+            var generator = new TypeGenerator();
+
+            var types = generator.Generate(parser.Run());
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
