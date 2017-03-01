c8d2ad02c87cb4768245fd311f2fe325927fa30a
Andrew Armstrong <FacticiusVir@gmail.com>
2017-03-01 02:04:13 +0000

    #32 Implemented additional bindings in Glfw rpoject

--- SharpVk/SharpVk.Glfw/Glfw3.cs
+++ SharpVk/SharpVk.Glfw/Glfw3.cs
@@ -8,10 +8,14 @@ namespace SharpVk.Glfw
         public const string GlfwDll = "glfw3.dll";
 
         [DllImport(GlfwDll, CallingConvention = CallingConvention.Cdecl)]
-        public static extern int glfwInit();
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool glfwInit();
 
         [DllImport(GlfwDll, CallingConvention = CallingConvention.Cdecl)]
         public static extern void glfwTerminate();
+
+        [DllImport(GlfwDll, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void glfwGetVersion(out int major, out int minor, out int rev);
 
         [DllImport(GlfwDll, CallingConvention = CallingConvention.Cdecl)]
         public static extern WindowHandle glfwCreateWindow(int width, int height, [MarshalAs(UnmanagedType.LPStr)] string title, IntPtr monitor, IntPtr share);
@@ -34,6 +38,37 @@ namespace SharpVk.Glfw
         [DllImport(GlfwDll, CallingConvention = CallingConvention.Cdecl)]
         public static extern WindowSizeDelegate glfwSetWindowSizeCallback(WindowHandle window, WindowSizeDelegate callback);
 
+        [DllImport(GlfwDll, CallingConvention = CallingConvention.Cdecl)]
+        public static extern ErrorDelegate glfwSetErrorCallback(ErrorDelegate callback);
+
+        [DllImport(GlfwDll, CallingConvention = CallingConvention.Cdecl)]
+        public static extern MonitorHandle* glfwGetMonitors(out int count);
+
+        [DllImport(GlfwDll, CallingConvention = CallingConvention.Cdecl)]
+        public static extern MonitorHandle glfwGetPrimaryMonitor();
+
+        [DllImport(GlfwDll, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void glfwGetMonitorPos(MonitorHandle monitor, out int xPos, out int yPos);
+
+        [DllImport(GlfwDll, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void glfwGetMonitorPhysicalSize(MonitorHandle monitor, out int widthMm, out int heightMm);
+
+        [DllImport(GlfwDll, CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.LPStr)]
+        public static extern string glfwGetMonitorName(MonitorHandle monitor);
+
+        [DllImport(GlfwDll, CallingConvention = CallingConvention.Cdecl)]
+        public static extern MonitorEventDelegate glfwSetMonitorCallback(MonitorEventDelegate callback);
+
+        [DllImport(GlfwDll, CallingConvention = CallingConvention.Cdecl)]
+        public static extern VideoMode* glfwGetVideoModes(MonitorHandle monitor, out int count);
+
+        [DllImport(GlfwDll, CallingConvention = CallingConvention.Cdecl)]
+        public static extern VideoModePointer glfwGetVideoMode(MonitorHandle monitor);
+
+        [DllImport(GlfwDll, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void glfwSetGamma(MonitorHandle monitor, float gamma);
+
         public static string[] glfwGetRequiredInstanceExtensions()
         {
             int count = 0;
@@ -45,6 +80,38 @@ namespace SharpVk.Glfw
             for (int nameIndex = 0; nameIndex < count; nameIndex++)
             {
                 result[nameIndex] = Marshal.PtrToStringAnsi(new IntPtr(namePointer[nameIndex]));
+            }
+
+            return result;
+        }
+
+        public static MonitorHandle[] glfwGetMonitors()
+        {
+            int count;
+
+            MonitorHandle* monitorPointer = glfwGetMonitors(out count);
+
+            var result = new MonitorHandle[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                result[i] = monitorPointer[i];
+            }
+
+            return result;
+        }
+
+        public static VideoMode[] glfwGetVideoModes(MonitorHandle monitor)
+        {
+            int count;
+
+            VideoMode* videoModePointer = glfwGetVideoModes(monitor, out count);
+
+            var result = new VideoMode[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                result[i] = videoModePointer[i];
             }
 
             return result;
