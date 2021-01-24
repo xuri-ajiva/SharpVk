31d85f91706d4d079f6c3e2e9c3c649590ebf7bc
xuri02 <t0bl4s.durst@gmail.com>
2021-01-24 22:42:46 +0100

    trying to fix som generation bugs

--- src/SharpVk.Generator/Specification/SimpleParser.cs
+++ src/SharpVk.Generator/Specification/SimpleParser.cs
@@ -1,85 +1,151 @@
-﻿using Sprache;
+﻿using System;
+using Sprache;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using SharpVk.Generator.Collation;
 
 namespace SharpVk.Generator.Specification
 {
     public static class SimpleParser
     {
-
         private static readonly Parser<LenExpression> lenExpressionParserRef = Parse.Ref(() => lenExpressionParser);
 
-        private static readonly Parser<LenExpressionToken> lenTokenParser = Parse.Letter.AtLeastOnce().Text().Select(x => new LenExpressionToken { Value = x });
+        private static readonly Parser<LenExpressionToken> lenTokenParser = Parse.Letter.AtLeastOnce().Text().Select(x => new LenExpressionToken
+        {
+            Value = x
+        });
 
         private static readonly Parser<LenExpressionReference> lenDereferenceParser = from left in lenTokenParser
-                                                                                         from op in Parse.String("::")
-                                                                                         from right in lenTokenParser
-                                                                                         select new LenExpressionReference
-                                                                                         {
-                                                                                             LeftOperand = left,
-                                                                                             RightOperand = right
-                                                                                         };
+            from op in Parse.String("::")
+            from right in lenTokenParser
+            select new LenExpressionReference
+            {
+                LeftOperand = left,
+                RightOperand = right
+            };
 
         private static readonly Parser<string> Sign = Parse.Char('-').Optional().Select(x => x.IsDefined ? "-" : "");
 
         private static readonly Parser<LenExpressionLiteral> integerLiteral = from sign in Sign
-                                                                                 from digits in Parse.Digit.AtLeastOnce().Text().Token()
-                                                                                 select new LenExpressionLiteral { Value = sign + digits };
+            from digits in Parse.Digit.AtLeastOnce().Text().Token()
+            select new LenExpressionLiteral
+            {
+                Value = sign + digits
+            };
 
         private static readonly Parser<LenExpressionToken> latexToken = Parse.LetterOrDigit.AtLeastOnce()
-                                                                                                 .Where(x => char.IsLetter(x.First()))
-                                                                                                 .Text()
-                                                                                                 .Token()
-                                                                                                 .Select(x => new LenExpressionToken { Value = x });
+            .Where(x => char.IsLetter(x.First()))
+            .Text()
+            .Token()
+            .Select(x => new LenExpressionToken
+            {
+                Value = x
+            });
 
         private static readonly Parser<LenExpression> latexSubexpressionRef = Parse.Ref(() => latexSubexpression);
 
-        private static readonly Parser<LenExpression> latexUnaryOperatorExpression = latexSubexpressionRef.Contained(Parse.String("\\lceil{"), Parse.String("}\\rceil")).Token().Select(x => new LenExpressionOperator { LeftOperand = x, Operator = LenOperatorType.Ceiling });
+        private static readonly Parser<LenExpression> latexUnaryOperatorExpression = latexSubexpressionRef.Contained(Parse.String("\\lceil{"), Parse.String("}\\rceil")).Token().Select(x => new LenExpressionOperator
+        {
+            LeftOperand = x,
+            Operator = LenOperatorType.Ceiling
+        });
 
         private static readonly Parser<LenExpression> latexPrimaryExpression = ((Parser<LenExpression>)latexToken)
-                                                                                                                                    .Or(latexToken.Contained(Parse.String("\\mathit{"), Parse.String("}")))
-                                                                                                                                    .Or(latexToken.Contained(Parse.String("\\textrm{"), Parse.String("}")))
-                                                                                                                                    .Or(integerLiteral)
-                                                                                                                                    .Or(latexUnaryOperatorExpression);
+            .Or(latexToken.Contained(Parse.String("\\mathit{"), Parse.String("}")))
+            .Or(latexToken.Contained(Parse.String("\\textrm{"), Parse.String("}")))
+            .Or(integerLiteral)
+            .Or(latexUnaryOperatorExpression);
 
         private static readonly Parser<LenExpression> latexMultiplicativeExpression = Parse.ChainOperator(Parse.String("\\over").Token(),
-                                                                                                                            latexPrimaryExpression,
-                                                                                                                            (op, left, right) => new LenExpressionOperator { LeftOperand = left, RightOperand = right, Operator = LenOperatorType.Divide });
+            latexPrimaryExpression,
+            (op, left, right) => new LenExpressionOperator
+            {
+                LeftOperand = left,
+                RightOperand = right,
+                Operator = LenOperatorType.Divide
+            });
 
         private static readonly Parser<LenExpression> latexSubexpression = latexMultiplicativeExpression;
 
         private static readonly Parser<string> latexExpressionStart = from start in Parse.String("latexmath:[").Text()
-                                                                      from dollar in Parse.Char('$').Optional()
-                                                                      select start;
+            from dollar in Parse.Char('$').Optional()
+            select start;
 
         private static readonly Parser<char> latexExpressionEnd = from dollar in Parse.Char('$').Optional()
-                                                                  from end in Parse.Char(']')
-                                                                  select end;
+            from end in Parse.Char(']')
+            select end;
 
         private static readonly Parser<LenExpression> latexExpression = latexSubexpression.Contained(latexExpressionStart, latexExpressionEnd);
 
         private static readonly Parser<LenExpression> lenExpressionParser = latexExpression
-                                                                                    .Or(lenDereferenceParser)
-                                                                                    .Or(lenTokenParser);
+            .Or(lenDereferenceParser)
+            .Or(lenTokenParser);
 
-        private static readonly Parser<MemberLen> lenPartParser = Parse.String("null-terminated").Select(x => new MemberLen { Type = LenType.NullTerminated })
-                                                                        .Or(lenExpressionParser.Select(x => new MemberLen { Value = x, Type = LenType.Expression }));
+        private static readonly Parser<MemberLen> lenPartParser = Parse.String("null-terminated").Select(x => new MemberLen
+            {
+                Type = LenType.NullTerminated
+            })
+            .Or(lenExpressionParser.Select(x => new MemberLen
+            {
+                Value = x,
+                Type = LenType.Expression
+            }));
 
         private static readonly Parser<IEnumerable<MemberLen>> lenParser = lenPartParser.DelimitedBy(Parse.Char(',').Token()).End();
 
         private static readonly Parser<string> fixedLengthParser = from open in Parse.Char('[')
-                                                                   from digits in Parse.Digit.AtLeastOnce().Text()
-                                                                   from close in Parse.Char(']')
-                                                                   select digits;
+            from digits in Parse.Digit.AtLeastOnce().Text()
+            from close in Parse.Char(']')
+            select digits;
 
         public static string ParseFixedLength(string value)
         {
+            value.StartsWith(":");
+            return value.Trim(':');
             return fixedLengthParser.Parse(value);
         }
 
         public static MemberLen[] ParsedLenField(string field)
         {
-            return lenParser.Parse(field).ToArray();
+            if (field.Contains("->d"))
+            {
+                return ParsedLenField( /*field.Split("->")[1]*/ "pDescriptorSets");
+            }
+            if (field.Contains("->c"))
+            {
+                return ParsedLenField( /*field.Split("->")[1]*/ "pCommandBuffers");
+            }
+            if (field.Contains("*"))
+            {
+                var parts = field.Split("*");
+                var multy = int.Parse(parts[0]);
+                int sizN = (parts[1])switch
+                {
+                    "VK_UUID_SIZE" => 128,
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+                return new[]
+                {
+                    new MemberLen
+                    {
+                        Value = new LenExpressionLiteral()
+                        {
+                            Value = (multy * sizN).ToString()
+                        },
+                        Type = LenType.Expression
+                    }
+                };
+            }
+            try
+            {
+                return lenParser.Parse(field).ToArray();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
 
         public static string NormaliseComment(string comment)
