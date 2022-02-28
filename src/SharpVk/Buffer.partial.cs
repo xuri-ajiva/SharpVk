f92029c731f96fb4cb601618b2a1b67478443265
xuri02 <t0bl4s.durst@gmail.com>
2022-02-28 22:01:54 +0100

    Some SharpVk Changes

--- src/SharpVk/Buffer.partial.cs
+++ src/SharpVk/Buffer.partial.cs
@@ -1,7 +1,31 @@
-﻿namespace SharpVk
+﻿using System;
+
+namespace SharpVk
 {
     partial class Buffer
-        : IMemoryBinder
+        : IMemoryBinder, IEquatable<Buffer?>
     {
+        /// <inheritdoc />
+        public bool Equals(Buffer other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Handle.Equals(other.Handle);
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Buffer)obj);
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            return Handle.GetHashCode();
+        }
     }
 }
